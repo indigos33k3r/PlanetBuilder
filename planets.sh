9ffe31ae39989a582f43b8c204826e9cf0e47dcf
phil jones <interstar@gmail.com>
2012-09-28 22:12:19 +0100

    changed script to pull MY fork of planetplanet from GitHub. Because I'm going to be making a couple of bug fixed / tweaks

--- planets.sh
+++ planets.sh
@@ -2,24 +2,26 @@
 
 mkdir $1
 cd $1
-curl http://www.planetplanet.org/download/planet-nightly.zip > planet.zip
-unzip planet.zip
-cp planet-nightly/examples/* .
-cp -r planet-nightly/examples/fancy .
-cp -r planet-nightly/examples/basic .
+curl -L https://github.com/interstar/planetplanet/zipball/master > planet.zip
 
-cp -r planet-nightly/examples/output/* .
+unzip planet.zip
+
+mv interstar-planetplanet* p2
+mv p2/* .
+rm -rf p2
+
+rm planet.zip
+cp -r examples/* .
+cp -r output/* .
 
 mv fancy/index.html.tmpl .
 
-rm planet.zip
-
-echo $(pwd)
+echo "Path here : $(pwd)"
 
 echo "#! /bin/sh" > refresh.sh
 
 echo "cd $(pwd)" >> refresh.sh
-echo "python ./planet-nightly/planet.py $(pwd)/fancy/config.ini" >> refresh.sh
+echo "python ./planet.py $(pwd)/fancy/config.ini" >> refresh.sh
 
 chmod +x refresh.sh
 
